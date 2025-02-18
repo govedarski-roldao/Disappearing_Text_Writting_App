903f532222c5a67d6ff8abfcd05091b3fc9b2c9b
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-02-18 22:21:56 +0200

    new day

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import Label
 
+# region UI
+root = Tk()
+root.title("Word Vanish")
+root.minsize(500, 300)
+root.config(padx=30, pady=30)
+
+window_title = Label(text="Practice your writing")
+window_title.grid(column=0, row=0, columnspan=3)
+
+text_field = Text(width=60, height=10, pady=10, padx=10)
+text_field.grid(column=0, row=2)
+
+root.mainloop()
+# endregion
