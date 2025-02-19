a1f960bef6ecaca75b652554e56d5a01fce5b35c
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-02-19 22:02:44 +0200

    end of project1

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-from time import sleep
 from tkinter import *
 from tkinter import Label
-import time
 
-time_left = 10
+
 words_before = 0
-words_after = 0
+
 
 from twilio.twiml.voice_response import Start
 
 
 def check_field():
     text = text_field.get("1.0", "end-1c")
-    words = text.split(" ")
-    text_length = len(words)
+    text_length = len(text)
     return text_length
 
 
 def count_time():
-    global time_left
-    global words
-    last_count = 0
-    while True:
-        new_count = check_field()
-        if new_count > last_count:
+    """Checks if new words are added every 10 seconds. If not, clears the text field."""
+    global words_before
+    new_count = check_field()
 
+    if new_count > words_before:
+        words_before = new_count  # Update word count
+        print(new_count)
+    else:
+        clean_field()
+        words_before = 0  # Reset count after clearing
 
-
+    root.after(5000, count_time)
 
 def clean_field():
-    global time_left
-    text_field.delete("1.0", "end-1c")
-    time_left = 10
+    text_field.delete("1.0", "end")
 
 
 # region UI
@@ -46,7 +44,7 @@ window_title.grid(column=0, row=0, columnspan=3)
 text_field = Text(width=60, height=10, pady=10, padx=10)
 text_field.grid(column=0, row=2)
 
-button_start = Button(text='Start', anchor='center', width=10, command=check_time)
+button_start = Button(text='Start', anchor='center', width=10, command=count_time)
 button_start.grid(column=0, row=3, pady=(5, 5), padx=(15, 5), sticky="e")
 
 root.mainloop()
